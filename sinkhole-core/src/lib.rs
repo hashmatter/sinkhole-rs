a06862613f79bbd7eeb3f262ddee1ed97b729a7c
Dean Eigenmann <7621705+decanus@users.noreply.github.com>
2020-04-10 16:32:36 +0200

    trait/storage (#10)
    * added storage
    
    * fix
    
    * fix

--- sinkhole-core/src/lib.rs
+++ sinkhole-core/src/lib.rs
@@ -1,1 +1,13 @@
+//! The core traits and types for the `sinkhole` library.
 
+/// A representation of the sinkhole database to query.
+///
+/// Implementations of this trait contain the
+/// servers private data addition and retrieval.
+pub trait Storage {
+    /// Adds content to the database.
+    fn add(&self, content: &[u8]);
+
+    /// Returns the data for a given ID.
+    fn retrieve(&self, id: [u8; 32]) -> &[u8];
+}
