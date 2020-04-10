fe6fb8e0c00200f030e6f98e8c20626d14862ac5
decanus <7621705+decanus@users.noreply.github.com>
2020-04-10 14:02:37 +0200

    replaced with vec

--- sinkhole-core/src/lib.rs
+++ sinkhole-core/src/lib.rs
@@ -1,5 +1,5 @@
 /// This type is a Private Key's byte value.
-pub type PrivateKey = [u8];
+pub type PrivateKey = Vec<u8>;
 
 /// This type is a Public Key's byte value.
-pub type PublicKey = [u8];
+pub type PublicKey = Vec<u8>;
