df555355043163097b0f6d664b4d983402d25f62
decanus <7621705+decanus@users.noreply.github.com>
2020-04-10 13:55:39 +0200

    fix

--- sinkhole-core/src/lib.rs
+++ sinkhole-core/src/lib.rs
@@ -6,8 +6,8 @@
 /// servers private data addition and retrieval.
 pub trait Storage {
     /// Adds content to the database.
-    fn add(&self, content: [u8]);
+    fn add(&self, content: &[u8]);
 
     /// Returns the data for a given ID.
-    fn retrieve(&self, id: [u8; 32]) -> [u8];
+    fn retrieve(&self, id: [u8; 32]) -> &[u8];
 }
