c24481579de904a1615d8803ea1870b0d951310b
decanus <7621705+decanus@users.noreply.github.com>
2020-04-10 13:53:13 +0200

    test

--- sinkhole-core/src/lib.rs
+++ sinkhole-core/src/lib.rs
@@ -1,1 +1,5 @@
+/// This type is a Private Key's byte value.
+pub type PrivateKey = [u8];
 
+/// This type is a Public Key's byte value.
+pub type PublicKey = [u8];
--- sinkhole-query/src/lib.rs
+++ sinkhole-query/src/lib.rs
@@ -1,1 +1,6 @@
+pub use sinkhole_core::{PublicKey, PrivateKey};
 
+pub trait Query {
+
+    fn encrypt(content: [u8], public_key: sinkhole_core::PublicKey);
+}
