0ee2633f67ee207395adf9b62fe798e00c08675c
decanus <7621705+decanus@users.noreply.github.com>
2020-04-10 14:11:02 +0200

    fmt

--- sinkhole-query/src/lib.rs
+++ sinkhole-query/src/lib.rs
@@ -1,6 +1,13 @@
-pub use sinkhole_core::{PublicKey, PrivateKey};
+//! The query generation and decryption used by the `sinkhole` library.
 
+pub use sinkhole_core::{PrivateKey, PublicKey};
+
+/// A trait used to encrypt queries and decrypt the results in
+/// various cryptographic schemes.
+///
+/// Implementations of this trait simply use different
+/// cryptography libraries for various trade-offs.
 pub trait Query {
-
-    fn encrypt(content: [u8], public_key: sinkhole_core::PublicKey);
+    fn encrypt(content: &[u8], public_key: sinkhole_core::PublicKey);
+    fn decrypt(result: &[u8], private_key: sinkhole_core::PrivateKey);
 }
