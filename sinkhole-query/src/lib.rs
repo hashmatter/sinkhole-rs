ea0aaeeb0a4492240f33f82e9dca7d55b54fee26
decanus <7621705+decanus@users.noreply.github.com>
2020-04-10 14:36:03 +0200

    added

--- sinkhole-query/src/lib.rs
+++ sinkhole-query/src/lib.rs
@@ -8,6 +8,9 @@ pub use sinkhole_core::{PrivateKey, PublicKey};
 /// Implementations of this trait simply use different
 /// cryptography libraries for various trade-offs.
 pub trait Query {
-    fn encrypt(content: &[u8], public_key: sinkhole_core::PublicKey);
-    fn decrypt(result: &[u8], private_key: sinkhole_core::PrivateKey);
+    /// Constructs an encrypted query.
+    fn construct(content: &[u8], public_key: sinkhole_core::PublicKey) -> &[u8];
+
+    /// Decrypts the result returned by the PIR server.
+    fn decrypt(result: &[u8], private_key: sinkhole_core::PrivateKey) -> &[u8];
 }
