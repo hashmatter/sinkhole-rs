0d8a005f638adf3e560ab0ff89b9b30f96d2d75e
gpestana <g6pestana@gmail.com>
2021-04-08 08:13:17 +0100

    Adds elgamal 0.0.1 benchmarks

--- benchmarks/benches/elgamal.rs
+++ benchmarks/benches/elgamal.rs
@@ -1,16 +1,157 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 
-use elgamal_ristretto::{private::SecretKey, public::PublicKey};
+use curve25519_dalek::scalar::Scalar;
 use rand_core::OsRng;
 
-pub fn criterion_benchmark(c: &mut Criterion) {
-        c.bench_function("generate key pair", |b| {
-            b.iter(|| {
-                let sk = SecretKey::new(&mut OsRng);
-                let _ = PublicKey::from(&sk);
-            })
-        });
+use elgamal_ristretto::{private::SecretKey, public::PublicKey};
+use sinkhole_core::traits::core::Storage as storage_trait;
+use sinkhole_elgamal::client::Query;
+use sinkhole_elgamal::storage::Storage;
+
+pub fn keypair_benchmark(c: &mut Criterion) {
+    c.bench_function("Generate key pair", |b| {
+        b.iter(|| {
+            let sk = SecretKey::new(&mut OsRng);
+            let _ = PublicKey::from(&sk);
+        })
+    });
 }
 
-criterion_group!(benches, criterion_benchmark);
+pub fn query_generation_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("query_gen_group");
+    group.sample_size(10);
+
+    let sk = SecretKey::new(&mut OsRng);
+
+    group.bench_function("Generate query size 2^5", |b| {
+        b.iter(|| {
+            let _ = Query::new(sk.clone(), 2usize.pow(5), 10);
+        })
+    });
+
+    group.bench_function("Generate query size 2^10", |b| {
+        b.iter(|| {
+            let _ = Query::new(sk.clone(), 2usize.pow(10), 10);
+        })
+    });
+
+    group.bench_function("Generate query size 2^15", |b| {
+        b.iter(|| {
+            let _ = Query::new(sk.clone(), 2usize.pow(15), 10);
+        })
+    });
+
+    group.bench_function("Generate query size 2^20", |b| {
+        b.iter(|| {
+            let _ = Query::new(sk.clone(), 2usize.pow(20), 10);
+        })
+    });
+}
+
+pub fn db_setup_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("db_setup_group");
+    let mut csprng = OsRng;
+
+    let sk = SecretKey::new(&mut OsRng);
+
+    group.bench_function("Setup DB size 2^5", |b| {
+        b.iter(|| {
+            let content: Vec<Scalar> = (1..2usize.pow(5) + 1)
+                .map(|_| Scalar::random(&mut csprng))
+                .collect();
+
+            let _ = Storage::new(sk.clone(), content.clone());
+        })
+    });
+
+    group.bench_function("Setup DB size 2^10", |b| {
+        b.iter(|| {
+            let content: Vec<Scalar> = (1..2usize.pow(10) + 1)
+                .map(|_| Scalar::random(&mut csprng))
+                .collect();
+
+            let _ = Storage::new(sk.clone(), content.clone());
+        })
+    });
+
+    group.bench_function("Setup DB size 2^15", |b| {
+        b.iter(|| {
+            let content: Vec<Scalar> = (1..2usize.pow(15) + 1)
+                .map(|_| Scalar::random(&mut csprng))
+                .collect();
+
+            let _ = Storage::new(sk.clone(), content.clone());
+        })
+    });
+
+    group.bench_function("Setup DB size 2^20", |b| {
+        b.iter(|| {
+            let content: Vec<Scalar> = (1..2usize.pow(20) + 1)
+                .map(|_| Scalar::random(&mut csprng))
+                .collect();
+
+            let _ = Storage::new(sk.clone(), content.clone());
+        })
+    });
+}
+
+pub fn run_query_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("run_query_group");
+    group.sample_size(10);
+
+    fn setup(
+        size: usize,
+    ) -> (
+        sinkhole_elgamal::storage::Storage,
+        sinkhole_elgamal::client::Query,
+    ) {
+        let mut csprng = OsRng;
+
+        let storage_sk = SecretKey::new(&mut OsRng);
+        let client_sk = SecretKey::new(&mut OsRng);
+
+        let content: Vec<Scalar> = (1..size + 1).map(|_| Scalar::random(&mut csprng)).collect();
+        let storage = Storage::new(storage_sk.clone(), content.clone());
+        let query = Query::new(client_sk.clone(), size, 10).unwrap();
+
+        (storage, query)
+    }
+
+    let (storage_2pow5, query_2pow5) = setup(2usize.pow(5));
+    let (storage_2pow10, query_2pow10) = setup(2usize.pow(10));
+    let (storage_2pow15, query_2pow15) = setup(2usize.pow(15));
+    let (storage_2pow20, query_2pow20) = setup(2usize.pow(20));
+
+    group.bench_function("Run query size 2^5", |b| {
+        b.iter(|| {
+            let _ = storage_2pow5.retrieve(query_2pow5.clone().encrypted);
+        })
+    });
+
+    group.bench_function("Run query size 2^10", |b| {
+        b.iter(|| {
+            let _ = storage_2pow10.retrieve(query_2pow10.clone().encrypted);
+        })
+    });
+
+    group.bench_function("Run query size 2^15", |b| {
+        b.iter(|| {
+            let _ = storage_2pow15.retrieve(query_2pow15.clone().encrypted);
+        })
+    });
+
+    group.bench_function("Run query size 2^20", |b| {
+        b.iter(|| {
+            let _ = storage_2pow20.retrieve(query_2pow20.clone().encrypted);
+        })
+    });
+}
+
+criterion_group!(
+    benches,
+    keypair_benchmark,
+    query_generation_benchmark,
+    db_setup_benchmark,
+    run_query_benchmark
+);
 criterion_main!(benches);
---
+++ benchmarks/results/elgamal-0.0.1/README.md
@@ -0,0 +1,3 @@
+## Sinkhole-elgamal 0.0.1 benchmark results
+
+[Criterion benchmark report](./report/index.html)
